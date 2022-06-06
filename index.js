f4bb6cf42c621d82ba5f45c27efd60fa2e23dcc2
weealDCTEF <elynetobr@outlook.com>
2022-06-06 10:18:10 -0300

    teste

--- index.js
+++ index.js
@@ -2,13 +2,21 @@ const express = require('express');
 const app   = express();
 const port = 3000;
 
-app.get('/',(req,res) =>{
-    res.send("Hello world!");
+const path = require('path');
+
+app.use(express.static(path.join(__dirname,"public"))); /*Definindo a pasta estática do express = public*/
+
+app.set('view engine','ejs'); /*express usa como view engine , o ejs.*/
+
+app.get('/',(req,res) => {
+    res.render('usuario/add.ejs');
 });
+
 
 app.listen(port,() =>{
     console.log("App rodando na porta:" + port);
 });
+
 
 
 
